e0724827df280c4f5329ce2f55ddbd2d22655be7
Deverell Manning <71154873+DeverellManning@users.noreply.github.com>
2024-03-18 14:10:21 -0600

    Add more Loader code

--- src/main/java/frc/robot/systems/Loader.java
+++ src/main/java/frc/robot/systems/Loader.java
@@ -13,6 +13,8 @@ public class Loader extends System {
     float loadSpeed = 0.5f;
     float intakeSpeed = 0.6f;
     int t;
+    boolean up = false;
+    boolean down = false;
     public Loader() {
         super();
         //Neo Motors (Brushless)
@@ -21,11 +23,16 @@ public class Loader extends System {
     }
 
     public void update() {
+    intakeMotor.set(0);
+    if (up == true || down == true){
+    t --;
+    }
 
     }
 
     public void lift() {
     t = 100; 
+    up = true;
     while (t != 0){
     angleMotor.set(loadSpeed);
     }
@@ -33,21 +40,23 @@ public class Loader extends System {
     if (t == 0){
     angleMotor.set(0);
     }
-
+    up = false;
     }
 
     public void spin(){
-
+    intakeMotor.set(intakeSpeed);
     }
 
     public void drop(){
-        t = 100; 
-        while (t != 0){
-        angleMotor.set(-loadSpeed);
-        }
-    
-        if (t == 0){
-        angleMotor.set(0);
-        }
+    down = true;
+    t = 100; 
+    while (t != 0){
+    angleMotor.set(-loadSpeed);
+    }
+
+    if (t == 0){
+    angleMotor.set(0);
+    }
+    down = false;
     }
 }
